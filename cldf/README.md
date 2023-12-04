ede604e7ba3b964e9546f013aa8b5ef42df3347f
lingulist <mattis.list@lingulist.de>
2023-12-04 22:07:33 +0100

    update new files

--- cldf/README.md
+++ cldf/README.md
@@ -1,3 +1,8 @@
+# CLDF datasets
+
+- [Wordlist](#ds-cldfmetadatajson)
+- [StructureDataset](#ds-structuredatasetmetadatajson)
+
 <a name="ds-cldfmetadatajson"> </a>
 
 # Wordlist CLDF dataset derived Giacomelli et al.'s "Atlante Lessicale Toscano" from 2000
@@ -12,9 +17,9 @@ property | value
 [dc:conformsTo](http://purl.org/dc/terms/conformsTo) | [CLDF Wordlist](http://cldf.clld.org/v1.0/terms.rdf#Wordlist)
 [dc:identifier](http://purl.org/dc/terms/identifier) | http://serverdbt.ilc.cnr.it/ALTWEB/
 [dc:license](http://purl.org/dc/terms/license) | https://creativecommons.org/licenses/by/4.0/
-[dcat:accessURL](http://www.w3.org/ns/dcat#accessURL) | https://github.com/lexibank/tuscandialects/
-[prov:wasDerivedFrom](http://www.w3.org/ns/prov#wasDerivedFrom) | <ol><li><a href="https://github.com/lexibank/tuscandialects//tree/23526a8">lexibank/tuscandialects/ 23526a8</a></li><li><a href="https://github.com/glottolog/glottolog/tree/2dfc4be1ed">Glottolog v4.8-43-g2dfc4be1ed</a></li><li><a href="https://github.com/concepticon/concepticon-data/tree/0d1fc1ae">Concepticon v3.1.0-80-g0d1fc1ae</a></li><li><a href="https://github.com/cldf-clts/clts/tree/6e60c1d">CLTS v2.2.0-11-g6e60c1d</a></li></ol>
-[prov:wasGeneratedBy](http://www.w3.org/ns/prov#wasGeneratedBy) | <ol><li><strong>lingpy-rcParams</strong>: <a href="./lingpy-rcParams.json">lingpy-rcParams.json</a></li><li><strong>python</strong>: 3.9.18</li><li><strong>python-packages</strong>: <a href="./requirements.txt">requirements.txt</a></li></ol>
+[dcat:accessURL](http://www.w3.org/ns/dcat#accessURL) | https://github.com/lexibank/tuscandialects
+[prov:wasDerivedFrom](http://www.w3.org/ns/prov#wasDerivedFrom) | <ol><li><a href="https://github.com/lexibank/tuscandialects/tree/4c1ee67">lexibank/tuscandialects 4c1ee67</a></li><li><a href="https://github.com/glottolog/glottolog/tree/v4.8">Glottolog v4.8</a></li><li><a href="https://github.com/concepticon/concepticon-data/tree/v3.1.0">Concepticon v3.1.0</a></li><li><a href="https://github.com/cldf-clts/clts//tree/v2.2.0">CLTS v2.2.0</a></li></ol>
+[prov:wasGeneratedBy](http://www.w3.org/ns/prov#wasGeneratedBy) | <ol><li><strong>lingpy-rcParams</strong>: <a href="./lingpy-rcParams.json">lingpy-rcParams.json</a></li><li><strong>python</strong>: 3.11.5</li><li><strong>python-packages</strong>: <a href="./requirements.txt">requirements.txt</a></li></ol>
 [rdf:ID](http://www.w3.org/1999/02/22-rdf-syntax-ns#ID) | tuscandialects
 [rdf:type](http://www.w3.org/1999/02/22-rdf-syntax-ns#type) | http://www.w3.org/ns/dcat#Distribution
 
@@ -94,4 +99,94 @@ Name/Property | Datatype | Description
 [Concepticon_ID](http://cldf.clld.org/v1.0/terms.rdf#concepticonReference) | `string` | 
 `Concepticon_Gloss` | `string` | 
 `Italian_Gloss` | `string` | 
+
+
+<a name="ds-structuredatasetmetadatajson"> </a>
+
+# StructureDataset CLDF dataset derived Giacomelli et al.'s "Atlante Lessicale Toscano" from 2000
+
+**CLDF Metadata**: [StructureDataset-metadata.json](./StructureDataset-metadata.json)
+
+**Sources**: [sources.bib](./sources.bib)
+
+property | value
+ --- | ---
+[dc:bibliographicCitation](http://purl.org/dc/terms/bibliographicCitation) | Giacomelli, G., Agostiniani, L., Bellucci, P., Giannelli, L., Montemagni, S., Nesi, A., Paoli, M., Picchi, E., and Poggi Salani, T. (eds) (2000). Atlante Lessicale Toscano. Roma: Lexis Progetti Editoriali.
+[dc:conformsTo](http://purl.org/dc/terms/conformsTo) | [CLDF StructureDataset](http://cldf.clld.org/v1.0/terms.rdf#StructureDataset)
+[dc:identifier](http://purl.org/dc/terms/identifier) | http://serverdbt.ilc.cnr.it/ALTWEB/
+[dc:license](http://purl.org/dc/terms/license) | https://creativecommons.org/licenses/by/4.0/
+[dcat:accessURL](http://www.w3.org/ns/dcat#accessURL) | https://github.com/lexibank/tuscandialects
+[prov:wasDerivedFrom](http://www.w3.org/ns/prov#wasDerivedFrom) | <ol><li><a href="https://github.com/lexibank/tuscandialects/tree/4c1ee67">lexibank/tuscandialects 4c1ee67</a></li><li><a href="https://github.com/glottolog/glottolog/tree/v4.8">Glottolog v4.8</a></li><li><a href="https://github.com/concepticon/concepticon-data/tree/v3.1.0">Concepticon v3.1.0</a></li><li><a href="https://github.com/cldf-clts/clts//tree/v2.2.0">CLTS v2.2.0</a></li></ol>
+[prov:wasGeneratedBy](http://www.w3.org/ns/prov#wasGeneratedBy) | <ol><li><strong>python</strong>: 3.11.5</li><li><strong>python-packages</strong>: <a href="./requirements.txt">requirements.txt</a></li></ol>
+[rdf:ID](http://www.w3.org/1999/02/22-rdf-syntax-ns#ID) | tuscandialects
+[rdf:type](http://www.w3.org/1999/02/22-rdf-syntax-ns#type) | http://www.w3.org/ns/dcat#Distribution
+
+
+## <a name="table-valuescsv"></a>Table [values.csv](./values.csv)
+
+property | value
+ --- | ---
+[dc:conformsTo](http://purl.org/dc/terms/conformsTo) | [CLDF ValueTable](http://cldf.clld.org/v1.0/terms.rdf#ValueTable)
+[dc:extent](http://purl.org/dc/terms/extent) | 11503
+
+
+### Columns
+
+Name/Property | Datatype | Description
+ --- | --- | --- 
+[ID](http://cldf.clld.org/v1.0/terms.rdf#id) | `string` | Primary key
+[Language_ID](http://cldf.clld.org/v1.0/terms.rdf#languageReference) | `string` | References [languages.csv::ID](#table-languagescsv)
+[Parameter_ID](http://cldf.clld.org/v1.0/terms.rdf#parameterReference) | `string` | References [features.csv::ID](#table-featurescsv)
+[Value](http://cldf.clld.org/v1.0/terms.rdf#value) | `string` | 
+[Code_ID](http://cldf.clld.org/v1.0/terms.rdf#codeReference) | `string` | 
+[Comment](http://cldf.clld.org/v1.0/terms.rdf#comment) | `string` | 
+[Source](http://cldf.clld.org/v1.0/terms.rdf#source) | list of `string` (separated by `;`) | References [sources.bib::BibTeX-key](./sources.bib)
+`Context` | `string` | 
+`Frequency` | `integer` | 
+
+## <a name="table-featurescsv"></a>Table [features.csv](./features.csv)
+
+property | value
+ --- | ---
+[dc:conformsTo](http://purl.org/dc/terms/conformsTo) | [CLDF ParameterTable](http://cldf.clld.org/v1.0/terms.rdf#ParameterTable)
+[dc:extent](http://purl.org/dc/terms/extent) | 82
+
+
+### Columns
+
+Name/Property | Datatype | Description
+ --- | --- | --- 
+[ID](http://cldf.clld.org/v1.0/terms.rdf#id) | `string` | Primary key
+[Name](http://cldf.clld.org/v1.0/terms.rdf#name) | `string` | 
+[Description](http://cldf.clld.org/v1.0/terms.rdf#description) | `string` | 
+[ColumnSpec](http://cldf.clld.org/v1.0/terms.rdf#columnSpec) | `json` | 
+`CLTS_BIPA` | `string` | 
+`CLTS_Name` | `string` | 
+`Lexibank_BIPA` | `string` | 
+`Frequency` | `integer` | 
+`OccurrencePerVariety` | `integer` | 
+`OccurrencePerConcept` | `integer` | 
+
+## <a name="table-languagescsv"></a>Table [languages.csv](./languages.csv)
+
+property | value
+ --- | ---
+[dc:conformsTo](http://purl.org/dc/terms/conformsTo) | [CLDF LanguageTable](http://cldf.clld.org/v1.0/terms.rdf#LanguageTable)
+[dc:extent](http://purl.org/dc/terms/extent) | 214
+
+
+### Columns
+
+Name/Property | Datatype | Description
+ --- | --- | --- 
+[ID](http://cldf.clld.org/v1.0/terms.rdf#id) | `string` | Primary key
+[Name](http://cldf.clld.org/v1.0/terms.rdf#name) | `string` | 
+[Glottocode](http://cldf.clld.org/v1.0/terms.rdf#glottocode) | `string` | 
+`Glottolog_Name` | `string` | 
+[ISO639P3code](http://cldf.clld.org/v1.0/terms.rdf#iso639P3code) | `string` | 
+[Macroarea](http://cldf.clld.org/v1.0/terms.rdf#macroarea) | `string` | 
+[Latitude](http://cldf.clld.org/v1.0/terms.rdf#latitude) | `decimal` | 
+[Longitude](http://cldf.clld.org/v1.0/terms.rdf#longitude) | `decimal` | 
+`Family` | `string` | 
+`Location` | `string` | 
 
--- lexibank_tuscandialects.py
+++ lexibank_tuscandialects.py
@@ -8,6 +8,9 @@ from pylexibank import FormSpec
 import re
 import codecs
 from collections import defaultdict, Counter
+from cldfbench import CLDFSpec
+from pyclts import CLTS
+import pylexibank
 
 
 @attr.s
@@ -64,78 +67,173 @@ class Dataset(BaseDataset):
                 f.write(num + "\t" + concept + "\n")
         args.log.info("wrote concepts in source to raw directory")
 
-
+    def cldf_specs(self):
+        return {
+            None: pylexibank.Dataset.cldf_specs(self),
+            "structure": CLDFSpec(
+                module="StructureDataset",
+                dir=self.cldf_dir,
+                data_fnames={"ParameterTable": "features.csv"},
+            ),
+        }
 
     def cmd_makecldf(self, args):
-        # add bib
-        args.writer.add_sources()
-        args.log.info("added sources")
-
-        # add concept
-        concepts = {}
-        for concept in self.concepts:
-            idx = concept["NUMBER"] + "_" + slug(concept["ITALIAN"])
-            args.writer.add_concept(
-                    ID=idx,
-                    Name=concept["ITALIAN"],
-                    Italian_Gloss=concept["ITALIAN"],
-                    )
-            concepts[concept["ITALIAN"]] = idx
-                    
-        args.log.info("added concepts")
-
-        # add language
-        languages = {}
-        for language in self.languages:
-            idx = language["ID"] + "_" + slug(language["Name"])
-            glottocode = "ital1282" if language["Name"] == "Italiano" else "fior1235"
-            isocode = "ita" if language["Name"] == "Italiano" else ""
-            args.writer.add_language(
-                ID=idx,
-                Name=language["Name"],
-                Longitude=language["Longitude"],
-                Latitude=language["Latitude"],
-                Glottocode=glottocode,
-                ISO639P3code=isocode
-                )
-            languages[language["ID"] + " " + language["Name"]] = idx
-        args.log.info("added languages")
-
-        # read in data
-        #data = self.raw_dir.read_csv(
-        #    "ALT-standardized_forms.csv", delimiter=","
-        #)
-
-        readings = defaultdict(lambda: defaultdict(list))
-        for path in self.raw_dir.glob("alt_notosc_IPA/*.fon"):
-            concept, language = "", ""
-            for row in self.raw_dir.read_csv(path):
-                line = row[0]
-                if line.startswith("%"):
-                    continue
-                elif line.startswith("#"):
-                    concept = line[2:].replace(" ", "_").replace("'", "_")
-                elif line.startswith(":"):
-                    language = line[2:]
-                elif line.startswith('-'):
-                    form = line[2:]
-                    readings[concept][language] += [form]
-
-        standardized_data = defaultdict(lambda: defaultdict(str))
-        for concept, langdict in readings.items():
-            for language, forms in langdict.items():
-                if not forms:
-                    continue
-                most_common_form = Counter(forms).most_common(1)[0][0]
-                standardized_data[concept][language] = most_common_form
         
-        # add data
-        for concept, langdict in pb(standardized_data.items(), desc="cldfify"):
-            for language, form in langdict.items():
-                if form.strip():
-                    args.writer.add_form(
-                            Language_ID=languages[language],
-                            Parameter_ID=concepts[concept],
-                            Value=form,
-                            Form=form,
-                            Source="tuscan")
+        sounds = defaultdict(
+                lambda : {
+                    "concept": defaultdict(int), 
+                    "variety": defaultdict(int), 
+                    "total": 0})
+        # add lexical data
+        with self.cldf_writer(args) as writer:
+            # add bib
+            writer.add_sources()
+            args.log.info("added sources")
+
+            # add concept
+            concepts = {}
+            for concept in self.concepts:
+                idx = concept["NUMBER"] + "_" + slug(concept["ITALIAN"])
+                writer.add_concept(
+                        ID=idx,
+                        Name=concept["ITALIAN"],
+                        Italian_Gloss=concept["ITALIAN"],
+                        )
+                concepts[concept["ITALIAN"]] = idx
+                        
+            args.log.info("added concepts")
+
+            # add languages
+            with open(self.raw_dir / "Tuscany-subset.kml") as f:
+                geodata = f.read()
+            pattern = re.compile(
+                    r'<Placemark>.*?<name>([0-9]+.*?)</name>.*?'
+                    '<LookAt>.*?<longitude>(.*?)</longitude>.*?'
+                    '<latitude>(.*?)</latitude>.*?</LookAt>.*?'
+                    '</Placemark>', re.DOTALL)
+            datapoints = re.findall(pattern, geodata)
+            # manually add coordinates for Standard Italian (from Glottolog)
+            datapoints.append(
+                    ("225 Italiano", "12.65", "43.05")
+                    )  
+            id_to_site_data = {}
+            
+            for site, long, lat in datapoints:
+                site_info = site.split()
+                site_id = site_info[0]
+                site_name = " ".join(site_info[1:]).replace("&apos;", "'")
+            
+                id_to_site_data[site_id] = {
+                    "name": site_name,
+                    "longitude": long,
+                    "latitude": lat
+                }
+            languages = {}
+            cldf_languages = []
+            for language in self.languages:
+                idx = language["ID"] + "_" + slug(language["Name"])
+                glottocode = "ital1282" if language["Name"] == "Italiano" else "fior1235"
+                isocode = "ita" if language["Name"] == "Italiano" else ""
+                lng = writer.add_language(
+                    ID=idx,
+                    Name=language["Name"],
+                    Longitude=id_to_site_data[language["ID"]]["longitude"],
+                    Latitude=id_to_site_data[language["ID"]]["latitude"],
+                    Glottocode=glottocode,
+                    ISO639P3code=isocode
+                    )
+                languages[language["ID"] + " " + language["Name"]] = idx
+                cldf_languages += [lng]
+            args.log.info("added languages")
+
+            readings = defaultdict(lambda: defaultdict(list))
+            for path in self.raw_dir.glob("alt_notosc_IPA/*.fon"):
+                concept, language = "", ""
+                for row in self.raw_dir.read_csv(path):
+                    line = row[0]
+                    if line.startswith("%"):
+                        continue
+                    elif line.startswith("#"):
+                        concept = line[2:].replace(" ", "_").replace("'", "_")
+                    elif line.startswith(":"):
+                        language = line[2:]
+                    elif line.startswith('-'):
+                        form = line[2:]
+                        readings[concept][language] += [form]
+
+            standardized_data = defaultdict(lambda: defaultdict(str))
+            for concept, langdict in readings.items():
+                for language, forms in langdict.items():
+                    if not forms:
+                        continue
+                    most_common_form = Counter(forms).most_common(1)[0][0]
+                    standardized_data[concept][language] = most_common_form
+            
+            # add data
+            for concept, langdict in pb(standardized_data.items(), desc="cldfify"):
+                for language, form in langdict.items():
+                    if form.strip():
+                        lexeme = writer.add_form(
+                                Language_ID=languages[language],
+                                Parameter_ID=concepts[concept],
+                                Value=form,
+                                Form=form,
+                                Source="tuscan")
+                        for sound in lexeme["Segments"]:
+                            sounds[sound]["variety"][languages[language]] += 1
+                            sounds[sound]["concept"][concepts[concept]] += 1
+                            sounds[sound]["total"] += 1
+            language_table = writer.cldf["LanguageTable"]            
+
+        with self.cldf_writer(args, cldf_spec="structure", clean=False) as writer:
+            # We share the language table across both CLDF datasets:
+            writer.cldf.add_component(language_table)
+            writer.objects["LanguageTable"] = cldf_languages
+            
+            # columns for the features table
+            writer.cldf.add_columns(
+                "ParameterTable",
+                {"name": "CLTS_BIPA", "datatype": "string"},
+                {"name": "CLTS_Name", "datatype": "string"},
+                {"name": "Lexibank_BIPA", "datatype": "string"},
+                {"name": "Frequency", "datatype": "integer"},
+                {"name": "OccurrencePerVariety", "datatype": "integer"},
+                {"name": "OccurrencePerConcept", "datatype": "integer"}
+            )
+            writer.cldf.add_columns(
+                    "ValueTable", 
+                    {"name": "Context", "datatype": "string"})
+            writer.cldf.add_columns(
+                    "ValueTable", {"name": "Frequency", "datatype": "integer"})
+            clts = CLTS(args.clts.dir)
+            bipa = clts.transcriptionsystem_dict["bipa"]
+            for sound_, values in pb(sounds.items(), desc="add sounds"):
+                sound = bipa[sound_]
+                pidx = "-".join(
+                        [s[2:] for s in sound.codepoints.split()])
+
+                writer.objects["ParameterTable"].append(
+                        {
+                            "ID": pidx,
+                            "Name": str(sound),
+                            "Description": sound.name,
+                            "CLTS_BIPA": str(sound),
+                            "CLTS_Name": sound.name,
+                            "Lexibank_BIPA": sound_,
+                            "Frequency": values["total"],
+                            "OccurrencePerVariety": len(values["variety"]),
+                            "OccurrencePerConcept": len(values["concept"])
+                        }
+                    )
+                for language, frequency in values["variety"].items():
+                    writer.objects["ValueTable"].append(
+                            {
+                                "ID": language + "_" + pidx,
+                                "Language_ID": language,
+                                "Parameter_ID": pidx,
+                                "Value": str(sound),
+                                "Frequency": frequency,
+                                "Source": ["tuscan"],
+                            }
+                        )
+                args.log.info("added sounds")
